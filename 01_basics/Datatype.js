ca21ac82d86abe5c0e1e28922a81afd0ad4b3965
Gaurav <khatrigarima073@gmail.com>
2025-01-06 11:57:09 +0530

    datatypes in javascript

---
+++ 01_basics/Datatype.js
@@ -0,0 +1,36 @@
+"use  strict";   //treat all js code as newer version
+
+// alert(3+3)       //we are using node.js not browser it is use in browser
+
+console.log(3+        //code readability should be high
+    3 );
+
+let name1="garima"
+let age=22 
+let isloggedIn=false 
+
+console.table([name1,age,isloggedIn])
+
+//----primitive data types
+// Number=for Integer value
+//bigint=for large Integer value
+//string =character
+//boolean=true/false
+//null=standalone value(empty)(Represents an intentional absence of value.)
+//undefined= let name(A variable declared but not assigned a value)
+//symbol=Represents a unique, immutable identifier.(introduced in ES6)
+
+//object is non-primitive data type
+
+
+/* Typeof in JavaScript is an operator used for type checking and returns the data type of the operand passed to it.*/
+console.log(typeof null);          //output:object
+console.log(typeof undefined);     //output:undefined
+console.log(typeof "garima");      //output:string
+
+
+
+
+
+
+
