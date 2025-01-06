48797d8fe7aca3a54789be3eef8feb2da27cf599
Gaurav <khatrigarima073@gmail.com>
2025-01-06 15:53:58 +0530

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,49 @@
+let score="garima"
+
+console.log(score);           //print value
+console.log(typeof score);    //print data type of value ya variable
+
+let valueInNumber=Number(score);       //string convert into number
+console.log(typeof valueInNumber);     //type is number
+
+console.log(valueInNumber);        //when string convert into number output is NaN(NOT A NUMBER)
+
+let stringnum="abc2002"
+console.log(stringnum);
+
+
+// "11"=11
+// "ABC11"=NaN(not a none)
+// true=1; false=0
+
+
+//CONVERSION TO BOOLEAN
+let isloggedIn="komal"
+
+let booleanisloggedIn=Boolean(isloggedIn);
+console.log(booleanisloggedIn);       //FALSE
+
+let valuenull=null;
+let nulltobool=Boolean(valuenull);
+console.log(nulltobool);           //false
+
+let emptystr=""
+let emptytoboolean=Boolean(emptystr)
+console.log(emptytoboolean);      //false
+
+
+
+
+// 1=>true ;0=>false
+// ""=>false
+//null=>false
+// "komal"=>true
+
+let someNumber=11
+let stringnumber=String(someNumber)      //number convert into string 
+console.log(stringnumber);               
+console.log(typeof stringnumber);         //STRING
+
+
+
+
