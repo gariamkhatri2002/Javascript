83c2f95ba32cb552b4e8850fe2b4e86aa3214c15
Gaurav <khatrigarima073@gmail.com>
2025-01-08 22:49:02 +0530

    Number and Maths in javascript

---
+++ 01_basics/Num_and_Math.js
@@ -0,0 +1,16 @@
+const marks=412;
+// console.log(marks);     //412
+
+const balance=new Number(500);
+// console.log(balance);         //[Number: 500]
+
+// console.log(balance.toString());     //(500)num convert to string
+// console.log(balance.toString().length);   //3    RETURN STRING LENGTH
+
+console.log(balance.toFixed(2));
+console.log(marks.toFixed(2));
+
+
+
+
+
