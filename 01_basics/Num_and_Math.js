8602be2164cb3c61a776a0da63a9cfae00893e10
Gaurav <khatrigarima073@gmail.com>
2025-01-09 22:17:09 +0530

    number and math in javascript

--- 01_basics/Num_and_Math.js
+++ 01_basics/Num_and_Math.js
@@ -7,8 +7,48 @@ const balance=new Number(500);
 // console.log(balance.toString());     //(500)num convert to string
 // console.log(balance.toString().length);   //3    RETURN STRING LENGTH
 
-console.log(balance.toFixed(2));
-console.log(marks.toFixed(2));
+// console.log(balance.toFixed(2));
+// console.log(marks.toFixed(2));
+
+const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++Maths++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));        //4
+// console.log(Math.round(4.6));     //5
+// console.log(Math.ceil(4.2));      //5
+// console.log(Math.floor(4.9));     //4
+// console.log(Math.min(4,2,8,6));   //2
+// console.log(Math.max(4,2,8,6));   //8
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
