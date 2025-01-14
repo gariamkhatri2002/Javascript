e1442d25f30479fc7731b585dc651da769dcd9a5
Gaurav <khatrigarima073@gmail.com>
2025-01-14 23:13:17 +0530

    functions in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,14 +27,51 @@ console.log("Result:",result);
 
 function loginUserMessage(username="sam"){
     if(!username){
-        console.log("please enter a username");
+        //console.log("please enter a username");
         return
         
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("garima"));
-console.log(loginUserMessage("garima"));
+// console.log(loginUserMessage("garima"));
+// console.log(loginUserMessage("garima"));
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(11,12,45,38));
+
+const user={
+    username:"garima",
+    priceS:199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([10,20,30,40]));
+
+
+
 
 
