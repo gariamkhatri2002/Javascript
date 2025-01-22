cc8a9121ba2a241a37d9f3ff8d124ddb8059a50b
Gaurav <khatrigarima073@gmail.com>
2025-01-22 23:57:26 +0530

    if else in javascript

---
+++ 01_basics/ConditionalExp.js
@@ -0,0 +1,20 @@
+const a=45;
+
+
+if(a>0 && a<18){
+    console.log("not valid for vote");
+}
+else if(a>=18){
+    console.log("valid age");
+    
+}
+else{
+    console.log("please enter valid age");
+    
+}
+
+
+// ternary operator
+const b=12
+console.log(18<b? "drive":"not drive");
+
